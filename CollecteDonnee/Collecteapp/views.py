c445a7e119aa2a7c1ea13aa309f558019e95dfd1
Guittet <og67guittet@gmail.com>
2023-06-21 23:29:21 +0200

    SAE

--- CollecteDonnee/Collecteapp/views.py
+++ CollecteDonnee/Collecteapp/views.py
@@ -13,11 +13,34 @@ def liste_capteurs(request):
     capteurs = Capteur.objects.all()
     return render(request, 'Collecteapp/Capteur/liste_capteur.html', {'capteurs': capteurs})
 
+
+def ajouter_capteur(request):
+    if request.method == 'POST':
+        form = CapteurForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'Collecteapp/confirmation.html')
+    else:
+        form = CapteurForm()
+
+    return render(request, 'Collecteapp/Capteur/ajouter_capteur.html',{'form': form})
+
 # Only for prod
-def modifier_capteur(request, id):
-    capteurs = Capteur.objects.get(pk=id)
+def modifier_capteur(request, di):
+    capteurs = Capteur.objects.get(pk=di)
     form = CapteurForm(request.POST or None, instance=capteurs)
     if form.is_valid():
         form.save()
         return HttpResponseRedirect("/Collecteapp/Capteur/liste")
     return render(request, 'Collecteapp/Capteur/modifier_capteur.html', {'form': form, 'capteurs': capteurs})
+
+
+def supprimer_capteur(request, di):
+    capteurs = Capteur.objects.get(pk=di)
+    return render(request, "Collecteapp/Capteur/supprimer_capteur.html", {"capteurs": capteurs})
+
+
+def supprimer_confirm_capteur(request, di):
+    capteurs = Capteur.objects.get(pk=di)
+    capteurs.delete()
+    return HttpResponseRedirect("/Collecteapp/Capteur/liste_capteur/")
