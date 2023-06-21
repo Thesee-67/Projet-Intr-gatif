adf68a20d8a00aa7bfd0c49e2403db76e92a4454
Guittet <og67guittet@gmail.com>
2023-06-21 16:41:25 +0200

    SAE

--- SAE24/SAE24/settings.py
+++ SAE24/SAE24/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'CollecteDonnee',
+    'CollecteDonnee.apps.CollecteDonneeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
