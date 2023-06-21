37a9893d04bbaab43f98b3fb2cf71ddf7e90a6e1
Guittet <og67guittet@gmail.com>
2023-06-21 16:31:21 +0200

    SAE

--- SAE24/SAE24/settings.py
+++ SAE24/SAE24/settings.py
@@ -75,17 +75,23 @@ WSGI_APPLICATION = 'SAE24.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.mysql',
+        #'NAME': 'CollecteDonnee',
+        #'USER': 'toto',
+        #'PASSWORD': 'toto',
+        #'HOST': 'localhost',
+        #'PORT': '3300',
+    #}
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'CollecteDonnee',
-        'USER': 'toto',
-        'PASSWORD': 'toto',
-        'HOST': 'localhost',
-        'PORT': '3300',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
--- SAE24/SAE24/urls.py
+++ SAE24/SAE24/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('CollecteDonnee/', include('CollecteDonnee.urls')),
-    path('', include('CollecteDonnee.urls')), 
+    path('CollecteDonnee/', include('CollecteDonnee.urls')), 
 ]
