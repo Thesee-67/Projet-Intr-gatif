fa20625f14ce14cc0ccbc684ab10e1ead51d3722
Guittet <og67guittet@gmail.com>
2023-06-22 10:16:29 +0200

    SAE

--- CollecteDonnee/Collecteapp/urls.py
+++ CollecteDonnee/Collecteapp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 
     path('Capteur/liste_capteur/', views.liste_capteurs),
     path('Capteur/ajouter_capteur', views.ajouter_capteur),
-    path('Capteur/modifier_capteur/<int:di>/', views.modifier_capteur),
-    path('Capteur/supprimer_capteur/<int:di>/', views.supprimer_capteur),
-    path('Capteur/supprimer_confirm_capteur/<int:di>/', views.supprimer_confirm_capteur),
+    path('Capteur/modifier_capteur/<str:di>/', views.modifier_capteur),
+    path('Capteur/supprimer_capteur/<str:di>/', views.supprimer_capteur),
+    path('Capteur/supprimer_confirm_capteur/<str:di>/', views.supprimer_confirm_capteur),
 ]
