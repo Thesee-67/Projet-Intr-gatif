ae42342c30b71badaa3a4722ad5a05187e945c28
Guittet <og67guittet@gmail.com>
2023-06-27 22:07:40 +0200

    SAE

--- MQTT/mqtt.py
+++ MQTT/mqtt.py
@@ -1,18 +1,14 @@
 import paho.mqtt.client as mqtt
 import mysql.connector
 from datetime import datetime
-import json
-
 broker = "test.mosquitto.org"
 topics = ["IUT/Colmar2023/SAE2.04/Maison1", "IUT/Colmar2023/SAE2.04/Maison2"]
 
 db_host = "localhost"
 db_user = "root"
+db_password = "toto"
 db_password = "04/11/17"
 db_name = "sae24"
-
-# Variable de données temporaires
-fichier_temporaire = "MQTT/donnees_temporaires.json"
 
 
 def on_connect(client, userdata, flags, rc):
@@ -23,22 +19,16 @@ def on_connect(client, userdata, flags, rc):
             client.subscribe(topic)
     else:
         print("Échec de la connexion. Code de retour =", rc)
-
-
 def on_message(client, userdata, msg):
-    retransmettre_donnees_temporaires()
     print("Message reçu. Topic =", msg.topic, " | Payload =", msg.payload.decode())
     db_connection = None
     cursor = None
-
     # Séparation des valeurs du payload
     payload_parts = msg.payload.decode().split(",")
     values = {}
     for part in payload_parts:
-        if "=" in part:
-            key, value = part.split("=")
-            values[key.strip()] = value.strip()
-
+        key, value = part.split("=")
+        values[key.strip()] = value.strip()
     try:
         db_connection = mysql.connector.connect(
             host=db_host,
@@ -46,14 +36,11 @@ def on_message(client, userdata, msg):
             password=db_password,
             database=db_name
         )
-
         cursor = db_connection.cursor()
-
         # Insertion du capteur s'il n'existe pas déjà
         sql_query_capteur = "INSERT IGNORE INTO capteur (id_capteur, PIECE) VALUES (%s, %s)"
-        cursor.execute(sql_query_capteur, (values.get("Id"), values.get("piece")))
+        cursor.execute(sql_query_capteur, (values.get("Id"), "Maison1"))
         db_connection.commit()
-
         # Conversion de la date au format anglais
         date_str = values.get("date")
         if date_str is not None:
@@ -67,56 +54,25 @@ def on_message(client, userdata, msg):
             print("Erreur: La valeur de date est manquante.")
             return
         
-        # Ajout de date_str et date_en dans le dictionnaire values
-        values["date_str"] = date_str
-        values["date_en"] = date_en
-
         # Insertion des données dans la table "details"
-        sql_query_details = "INSERT INTO Donnees (id_capteur_id, date, time, temps) VALUES (%s, %s, %s, %s)"
-        cursor.execute(sql_query_details, (values.get("Id"), values.get("date_en"), values.get("time"), values.get("temp")))
+        sql_query_details = "INSERT IGNORE INTO Capteur (id_capteur, piece) VALUES (%s, %s) ON DUPLICATE KEY UPDATE PIECE=VALUES(PIECE)"
+        cursor.execute(sql_query_details, (values.get("Id"), values.get("piece")))
         db_connection.commit()
-
+        sql_query_details = "INSERT INTO Donnees (id_capteur_id, date, time, temps) VALUES (%s, %s, %s, %s)"
+        cursor.execute(sql_query_details, (values.get("Id"),date_en, values.get("time"), values.get("temp")))
+        db_connection.commit()
         print("Données insérées avec succès dans la base de données.")
-
     except mysql.connector.Error as error:
         print("Erreur lors de la connexion à la base de données:", error)
-        # Stocker temporairement les données dans un fichier
-        donnees_temporaires = []
-        try:
-            with open(fichier_temporaire, "r") as file:
-                donnees_temporaires = json.load(file)
-        except FileNotFoundError:
-            pass
-        donnees_temporaires.append(values)
-        with open(fichier_temporaire, "w") as file:
-            json.dump(donnees_temporaires, file)
-
     finally:
         if cursor is not None:
             cursor.close()
         if db_connection is not None and db_connection.is_connected():
             db_connection.close()
-
-
-def retransmettre_donnees_temporaires():
-    try:
-        with open(fichier_temporaire, "r") as file:
-            donnees_temporaires = json.load(file)
-            if donnees_temporaires:
-                client.publish("IUT/Colmar2023/SAE2.04/Maison1", json.dumps(donnees_temporaires))
-    except FileNotFoundError:
-        pass
-
-
+    
 client = mqtt.Client()
-
 client.on_connect = on_connect
 client.on_message = on_message
-
 # Connexion au broker MQTT
 client.connect(broker, 1883, 60)
-
-# Retransmettre les données temporaires au redémarrage
-retransmettre_donnees_temporaires()
-
 client.loop_forever()
